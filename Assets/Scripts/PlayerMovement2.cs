3fa8dc5bf94a7fc69596416d0eaaa2d5910a808e
Brian Mosquera <149270404+bmucf@users.noreply.github.com>
2025-10-19 13:41:22 -0400

    tweaked values

--- Assets/Scripts/PlayerMovement2.cs
+++ Assets/Scripts/PlayerMovement2.cs
@@ -5,51 +5,53 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement2 : MonoBehaviour
 {
-    public float moveSpeed, turnRadius;
-
-    public Vector3 forwardDirection = Vector3.forward;
-
     public Rigidbody playerRB;
 
-    bool isMoving;
-    bool isGrounded;
-    bool isTricking;
+    public float forwardAcceleration, reverseAcceleration, maxSpeed, turnInput, turnStrength;
+
+    public Vector3 forwardDirection, movementDirection;
+
+    bool isMoving, isGrounded, isTricking;
 
     private void Awake()
     {
-        moveSpeed = 20f;
-        turnRadius = 10f;
+        forwardAcceleration = 5f;
+        maxSpeed = 20f;
+
+        turnStrength = 10f;
 
         playerRB = GetComponent<Rigidbody>();
         playerRB.freezeRotation = true;
-        playerRB.maxLinearVelocity = 100f;
-    }
-
-    private void Start()
-    {
+        playerRB.maxLinearVelocity = maxSpeed;
     }
 
     private void Update()
     {
+        turnInput = Input.GetAxis("Horizontal");
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime, 0f));
 
+        // defines "forward" as the direction the player is facing
+        // should be mostly independent from actual movement direction
+        // for example when rotating in the air
+        forwardDirection = transform.forward;
+        movementDirection = transform.forward; // placeholder
     }
 
     private void FixedUpdate()
     {
         AutoForward();
-        RotateCheck();
     }
-
 
     public void AutoForward()
     {
-        // provides an acceleration: 
-        Vector3 facingDirection = transform.forward.normalized;
-        playerRB.linearVelocity += forwardDirection * moveSpeed * Time.deltaTime;
-    }
+        // accelerates the player along its local forward axis
+        playerRB.AddForce(transform.forward * forwardAcceleration, ForceMode.Acceleration);
+        Debug.Log(playerRB.linearVelocity.magnitude);
 
-    public void RotateCheck()
-    {
-
+        // clamps speed
+        if (playerRB.linearVelocity.magnitude > maxSpeed)
+        {
+            playerRB.linearVelocity = forwardDirection * maxSpeed;
+        }
     }
 }
