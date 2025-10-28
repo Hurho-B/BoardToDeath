2bf58df19fe811fc889d4eba4eccb1abd366d3ba
Brian Mosquera <149270404+bmucf@users.noreply.github.com>
2025-10-28 19:04:10 -0400

    worked on ui menus

--- Assets/Scripts/UserInterfaceManager.cs
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class UserInterfaceManager : MonoBehaviour
 {
     public GameObject startMenu;
     public GameObject pauseMenu;
