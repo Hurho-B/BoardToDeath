57a17bf41c449dffaabb57daffeb4637b4535417
Jordan Norton <nortonjstone@gmail.com>
2025-11-05 17:24:31 -0500

    Added music player
    Functional music player with playlist shuffling capabilities

--- Assets/Scripts/MusicHandler.cs
+++ Assets/Scripts/MusicHandler.cs
@@ -4,31 +4,43 @@ using UnityEngine;
 public class MusicHandler : MonoBehaviour
 {
     public AudioSource MusicPlayer;
-    public AudioClip[] Song = new AudioClip[10];
+    public AudioClip[] Songs = new AudioClip[10];
+
+    public int songIndex = 0;
 
     public bool shouldPlayMusic = false;
 
+    void Start()
+    {
+        shuffle(Songs);
+    }
+
+    void Update()
+    {
+        if (!MusicPlayer.isPlaying)
+            play(Songs);
+    }
+
     public void play(AudioClip[] s)
     {
-        int songIndex = 0;
-
-        shouldPlayMusic = true;
-
-        shuffle(s);
-
         //Play music starting with first song in the array
-        while (shouldPlayMusic)
+        if (shouldPlayMusic)
         {
+            //Only play a song if the music player is not already playing a song
             if (!MusicPlayer.isPlaying)
             {
-                if ((songIndex > s.Length) || (songIndex < 0))
+                //Reshuffle and start over if the current playlist has already been played
+                if ((songIndex >= s.Length) || (songIndex < 0))
                 {
-                    shuffle(s);
                     songIndex = 0;
+                    shuffle(Songs);
                 }
 
                 MusicPlayer.PlayOneShot(s[songIndex]);
             }
+
+            //Increment song index so the next song will be played
+            songIndex++;
         }
     }
 
@@ -48,7 +60,7 @@ public class MusicHandler : MonoBehaviour
         //Set the contents of the original song array to that of the newly-shuffled array 
         for (int i = 0; i < s.Length; i++)
         {
-            Song[i] = s[i];
+            Songs[i] = s[i];
         }
     }
 }
